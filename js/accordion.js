07b6e15da1dd7550e04a4a890ce1583a2754579e
Warrme <jwnorm2@gmail.com>
2022-02-21 19:03:16 -0800

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$('.accordion').on('click', '.accordion-control', function(e){
+	e.preventDefault();
+	$(this)
+		.next('.accordion-panel')
+		.not(':animated')
+	.slideToggle();
+	
+});
